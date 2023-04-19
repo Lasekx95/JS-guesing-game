140b53e792c9d08ac7d26a3eea6a5d41cca970b4
Dustin <ddemers1995@hotmail.com>
2023-04-18 19:19:55 -0700

    finished game

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
+// Using parseInt to return NaN if user adds a string to the prompt
+// Creates prompt for max number
 let maximum = parseInt(prompt('Enter the maximum number!'));
+
+// Prompts user to add a valid number
 while (!maximum) {
     maximum = parseInt(prompt('Enter a valid number'));
 }
 
-Math.floor(Math.random() * maximum) + 1;
+// Uses random to create our random number, and floor to get rid of the decimal
+const targetNum = Math.floor(Math.random() * maximum) + 1;
 
+// Prompts the first guess
 let guess = parseInt(prompt('Enter your first guess!'));
-while (guess !== targetNum) {
+let attempts = 1;
 
+// Gameloop begins
+while (parseInt(guess) !== targetNum) {
+    if (guess === 'q') break; // type 'q' to quit
+    attempts++; // loops attempts and records them
+    if (guess > targetNum) { // Guess is lower, returns prompt
+        guess = prompt('Too high! Enter a new guess:');
+    } else {
+        guess = prompt('too high! Enter a new guess:'); // Returns prompt if guess is higher
+    }
+}
+if (guess === 'q') { // conditional to display win/quit msg
+    console.log('You quit');
+} else {
+    console.log(`You got it! it took you ${attempts} tries`);
 }
